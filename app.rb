9c25bff544ac2f169f4e25962931b2adf149f555
Sebastian Trujillo <juanstt99@gmail.com>
2023-05-12 16:11:04 -0500

    update routing

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'pg'
 require 'dotenv/load'
+require_relative 'controllers/drivers_controller'
+require_relative 'controllers/riders_controller'
+require_relative 'controllers/rides_controller'
 
 db_params = {
   host: ENV['DB_HOST'],
@@ -11,6 +14,15 @@ db_params = {
 }
 
 db = PG::Connection.new(db_params)
+
+# Rutas de riders
+use RidersController
+
+# Rutas de drivers
+use DriversController
+
+# Rutas de drivers
+use RidesController
 
 get '/' do
   'API'
